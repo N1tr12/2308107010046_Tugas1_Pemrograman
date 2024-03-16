30d94828b6257d720fe53800ba0fc8680a571806
N1tr12 <113963683+N1tr12@users.noreply.github.com>
2024-03-16 17:56:01 +0700

    Add files via upload

---
+++ 2308107010046_1.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+
+int main() {
+    int pilih, bilangan, desimal, biner = 0, oktal = 0, basis = 1, digit;
+
+    printf("Pilih Metode Konversi:\n1. Desimal ke Biner\n2. Biner ke Desimal\n3. Desimal ke Oktal\n4. Oktal ke Desimal\nPilih: ");
+    scanf("%d", &pilih);
+
+    switch (pilih) {
+        case 1:
+            printf("Masukkan bilangan Desimal: ");
+            scanf("%d", &bilangan);
+            desimal = bilangan;
+            while (bilangan > 0) {
+                digit = bilangan % 2;
+                biner += digit * basis;
+                bilangan /= 2;
+                basis *= 10;
+            }
+            printf("Hasil konversi ke Biner adalah : %d\n", biner);
+            break;
+        
+        case 2:
+            printf("Masukkan bilangan biner: ");
+            scanf("%d", &bilangan);
+            basis = 1;
+            while (bilangan > 0) {
+                digit = bilangan % 10;
+                if (digit != 0 && digit != 1) {
+                    printf("Bilangan bukan biner.\n");
+                    return 1;
+                }
+                desimal += digit * basis;
+                bilangan /= 10;
+                basis *= 2;
+            }
+            printf("Hasil konversi ke Desimal: %d\n", desimal);
+            break;
+        
+        case 3:
+            printf("Masukkan bilangan Desimal: ");
+            scanf("%d", &bilangan);
+            desimal = bilangan;
+            basis = 1;
+            while (bilangan > 0) {
+                digit = bilangan % 8;
+                oktal += digit * basis;
+                bilangan /= 8;
+                basis *= 10;
+            }
+            printf("Hasil konversi ke Oktal : %d\n", oktal);
+            break;
+        
+        case 4:
+            printf("Masukkan bilangan Oktal: ");
+            scanf("%d", &bilangan);
+            basis = 1;
+            while (bilangan > 0) {
+                digit = bilangan % 10;
+                if (digit >= 8) {
+                    printf("Bilangan bukan Oktal!\n");
+                    return 1;
+                }
+                desimal += digit * basis;
+                bilangan /= 10;
+                basis *= 8;
+            }
+            printf("Hasil konversi ke Desimal : %d\n", desimal);
+            break;
+
+        default:
+            printf("Pilihan tidak tersedia!\n");
+            return 1;
+    }
+
+    return 0;
+}
